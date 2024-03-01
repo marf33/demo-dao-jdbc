d0ac49f3c594df51ad0bd97b729838b4aa0abe96
Miguel Figueiredo <miguel.figueiredo96@gmail.com>
2024-03-01 14:31:53 +0000

    findbyId implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
 	public static SellerDao createSellerDao() {
 		//Instancia uma implementação
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 }
